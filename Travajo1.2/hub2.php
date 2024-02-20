4228f0acce4c960ff786158c55476cb953ff0294
chava-PA <159972658+chava-PA@users.noreply.github.com>
2024-02-20 00:47:35 -0600

    Add files via upload

---
+++ Travajo1.2/hub2.php
@@ -0,0 +1,62 @@
+<html>
+    <body>
+     <h1 align="center">12655. C09</h1>
+     
+     <h2>DESCRIPCION</h2>
+     <p>
+     Escribe un programa que solicite las calificaciones de Matemáticas,
+      Español y Cocina y nos de su promedio.
+
+            
+    </p>
+
+    
+    <h2>ENTRADA</h2>
+    <p>Tres números reales
+    </p>
+    <h2>SALIDA</h2>
+    <p>Un número real representado el promedio de los tres números</p>
+    <h2>EJEMPLO</h2>
+
+
+    
+    
+    
+
+
+    <table border="1">
+        <tr>
+            <td><h4>ENTRADA</h4></td>
+         
+
+
+            <td><h4>SALIDA</h4></td>
+        </tr>
+        <tr >
+            <td align="center">10 9.5 9.0 </td>
+            <td align="center"><?php
+
+            # CODIGO PHP PARA LA MULTIPLICACION
+            $a=10;
+            $b=9.5;
+            $c=9.0;
+
+            # OPERACION
+            $m=$a+$b+$c;
+            $v=$m/3;
+
+            echo "<br>calificacion final :", $v;
+            
+        
+            
+            ?></td>
+        </tr>
+        
+       
+        
+        
+    </table>
+
+
+    </body>
+</html>
