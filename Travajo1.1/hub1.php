325a0a31ad721eb405628faacfa87b759b7bddba
chava-PA <159972658+chava-PA@users.noreply.github.com>
2024-02-20 02:35:52 -0600

    Add files via upload

---
+++ Travajo1.1/hub1.php
@@ -0,0 +1,83 @@
+<html>
+    <body>
+     <h1 align="center">12662. C16</h1>
+     
+     <h2>DESCRIPCION</h2>
+     <p>
+        OMIJal se ha encontrado con una variante
+        en el Concurso ACM, pues el numero de programadores que conforman
+        un equipo puede variar, entonces modifica tu programa anterior pues 
+        ahora deberás preguntar el numero de 3 integrantes por equipo, al final 
+        indica cuantos equipos puedenformarse y cuantos programadores quedan fuera
+            
+    </p>
+
+    
+    <h2>ENTRADA</h2>
+    <p>Dos números que representen el número de programadores
+       y el número de programadores por equipo
+    </p>
+    <h2>SALIDA</h2>
+    <p>Salida con el número de equipos formados y los estudiantes sin equipo,
+        escrito exactamente como en el ejemplo</p>
+    <h2>EJEMPLO</h2>
+
+
+    
+    
+    
+
+
+    <table border="1">
+        <tr>
+            <td><h4>ENTRADA</h4></td>
+         
+
+
+            <td><h4>SALIDA</h4></td>
+        </tr>
+        <tr >
+            <td align="center">5<br> 4 </td>
+            <td align="center"><?php
+
+            # CODIGO PHP PARA LA MULTIPLICACION
+            $a=5;
+            $b=4;
+
+            # OPERACIONS
+            $m=$a/$b;
+            $v=$a%$b;
+            
+            echo "<br>Equipos formado:", round($m,0);
+            echo "<br>Estudiantes sin equuipo:", $v;
+        
+            
+            ?></td>
+        </tr>
+        <tr >
+            <td align="center">18<br> 3</td>
+            <td align="center"><?php 
+            
+            # CODIGO PHP PARA LA MULTIPLICACION
+            $a=18;
+            $b=3;
+
+            # OPERACION
+            $m=$a/$b;
+            $v=$a%$b;
+            
+            echo "<br>Equipos formado:", round($m,0);
+            echo "<br>Estudiantes sin equuipo:", $v;
+        
+            
+            
+            ?></td>
+        </tr>
+    
+        
+        
+    </table>
+
+
+    </body>
+</html>
